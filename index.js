1ae19174c0d221d8b86047f618fb841fb24d6f22
katyamar22 <katyamarcam@gmail.com>
2024-06-06 16:19:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Declare packages (express, body parser, and nodemon)
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+// Declare port
+const port = 3000;
+
+// GET => retrieve all entries from the database
+app.get((req, res) => {
+
+})
+
+// POST => Add a new entry to the database
+app.post((req, res) => {
+
+})
+
+// PUT => 
+app.put((req, res) => {
+
+})
+
+// DELETE
+app.delete((req, res) => {
+
+})
