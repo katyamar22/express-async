a76889fcc696a21153a4203f762d13b2c4b893d9
katyamar22 <katyamarcam@gmail.com>
2024-06-06 17:17:31 -0400

    Updated express-async code

--- index.js
+++ index.js
@@ -1,27 +1,113 @@
 // Declare packages (express, body parser, and nodemon)
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser')
+
+const app = express();
 
 // Declare port
 const port = 3000;
 
-// GET => retrieve all entries from the database
-app.get((req, res) => {
+app.use(bodyParser.json());
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+// MOCK DATABASE
+let data = [
+    {
+        id: 1,
+        fullName: 'John Doe',
+        position: 'Software Engineer'
+    },
+    {
+        id: 2,
+        fullName: 'Chris Wilson',
+        position: 'Secretary'
+    },
+    {
+        id: 3,
+        fullName: 'Nivid Tylers',
+        position: 'Head of HR'
+    },
+    {
+        id: 4,
+        fullName: 'Cecila Garcia',
+        position: 'HR'
+    },
+    {
+        id: 5,
+        fullName: 'Albert Brown',
+        position: 'Data Scientest'
+    },
+    {
+        id: 6,
+        fullName: 'Tyler Johnson',
+        position: 'Marketing'
+    },
+    {
+        id: 7,
+        fullName: 'Darien Culp',
+        position: 'Software Engineer'
+    },
+    {
+        id: 8,
+        fullName: 'Kyra River',
+        position: 'Project Manage'
+    },
+    {
+        id: 9,
+        fullName: 'Jeremy Smith',
+        position: 'Finance'
+    },
+    {
+        id: 10,
+        fullName: 'Alicia White',
+        position: 'Accountant'
+    },
+];
+
+// GET => retrieve all entries from the database
+app.get('/api/data',(req, res) => {
+    setTimeout(() => {
+        res.json(data);
+    }, 1000) // DELAY
 })
 
 // POST => Add a new entry to the database
-app.post((req, res) => {
-
+app.post('api/data',(req, res) => {
+    const newData = req.body;
+    setTimeout(() => {
+        data.push(newData);
+        res.status(201).send('Data added succesfully');
+    }, 1000) //DELAY
 })
 
 // PUT => 
-app.put((req, res) => {
-
+app.put('api/data/:id',(req, res) => {
+    const id = parseInt(req.params.id);
+    const updateData = req.body;
+    setTimeout(() => {
+        const index = data.findIndex( item => item.id === id);
+        if (index !== -1) {
+            db[index] = updateData;
+            res.status('Data updated succesfully');
+        } else {
+            res.status(404).send('Data not found');
+        }
+    }, 1000) 
 })
 
 // DELETE
-app.delete((req, res) => {
-
-})
+app.delete('api/data/:id',(req, res) => {
+    const id = parseInt(req.params.id);
+    setTimeout(() => {
+        const index = db.findIndex(item => item.id === id);
+        if (index !== -1) {
+                data.splice(index, 1);
+                res.send('Data deleted successfully');
+            } else {
+                res.status(404).send('Data not found');
+            }
+        }, 1000)
+    });
